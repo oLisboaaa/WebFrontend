22363a6407b5c2f8ceb564ea8abb6a15dab56e0f
Marcelo Lisboa <161672293+oLisboaaa@users.noreply.github.com>
2025-09-01 22:13:36 -0300

    .

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {Home} from './home/home';
+import { Home } from './home/home';
 import { Calculadora } from './calculadora/calculadora';
 import { ListarCliente } from './listar-cliente/listar-cliente';
 import { Pagenotfound } from './pagenotfound/pagenotfound';
+import { Telapai } from './telapai/telapai';
+import { Telafilho1 } from './telafilho1/telafilho1';
+import { Telafilho2 } from './telafilho2/telafilho2';
 
 
 const routes: Routes = [
-  {path: '', redirectTo:'/home', pathMatch: 'full'},
-  {path: 'home', component: Home},
-  {path: 'calculadora', component: Calculadora},
-  {path: 'listar-cliente', component: ListarCliente},
-  {path: '**', component: Pagenotfound}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'calculadora', component: Calculadora },
+  { path: 'listar-cliente', component: ListarCliente },
+  {
+    path: 'tela-pai', component: Telapai, children: [
+      { path: 'tela-filho1', component: Telafilho1 },
+      { path: 'tela-filho2', component: Telafilho2 }
+    ]
+  },
+  { path: '**', component: Pagenotfound }
 ];
 
 @NgModule({
